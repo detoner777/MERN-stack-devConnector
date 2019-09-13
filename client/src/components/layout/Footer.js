30fdac391917da116a2fabc4210b3267b39f3f67
Alexey_Lisenko <detoner777@gmail.com>
2019-09-13 11:05:33 +0300

    Bugs fix on layout css

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,35 @@
+// No nead to use this component now
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer-distributed">
+      <div className="footer-right">
+        <a href="#">
+          <i className="fa fa-facebook"></i>
+        </a>
+        <a href="#">
+          <i className="fa fa-twitter"></i>
+        </a>
+        <a href="#">
+          <i className="fa fa-linkedin"></i>
+        </a>
+        <a href="#">
+          <i className="fa fa-github"></i>
+        </a>
+      </div>
+
+      <div className="footer-left">
+        <p className="footer-links">
+          <a href="#">Home</a>·<a href="#">Blog</a>·<a href="#">Pricing</a>·
+          <a href="#">About</a>·<a href="#">Faq</a>·<a href="#">Contact</a>
+        </p>
+
+        <p>Dev Connector 2019</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
