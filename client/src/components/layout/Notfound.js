59bf592b0494ec184da7694c01c7a6f11b52c0f7
Alexey_Lisenko <detoner777@gmail.com>
2019-09-16 18:26:19 +0300

    RouteNotFound component and separete Routes component

---
+++ client/src/components/layout/Notfound.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const Notfound = () => {
+  return (
+    <Fragment>
+      <h1 className="x-large text-primary">
+        <i className="fas fa-exclamation-triangle"></i>Page Not Found
+      </h1>
+    </Fragment>
+  );
+};
+
+export default Notfound;
